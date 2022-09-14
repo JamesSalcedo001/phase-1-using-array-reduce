164ad5c52fa6fde274b20746c61d2700adf80b05
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-09-13 22:38:45 -0500

    commit tree 768e01bc9d19a58119aade402d4b0ca6a264cc7c

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+// this might be the key to the first one
+
+// function sum(nums) {
+//     let total = 0;
+//     nums.forEach(function(i) {
+//         total = total + i;
+//     });
+//     return total;
+// }
+// undefined
+// sum([5, 10, 15]);
+// 30
+
+const totalBatteries = batteryBatches.reduce((accum, value) => {
+    return accum + value;
+}, 0)
